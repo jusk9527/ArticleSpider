3a854aa4bb806c1146a7926b73dade2bd6f61e12
jusk <15507925675xiao@gmail.com>
2020-05-23 10:47:59 +0800

    update

--- demo.py
+++
@@ -1,7 +0,0 @@
-def foo(num):
-    print("starting...")
-    while num<10:
-        num=num+1
-        yield num
-for n in foo(0):
-    print(n)
--- rm_docker.sh
+++
@@ -1,4 +0,0 @@
-docker stop $(docker ps -a -q) 或者 docker stop $(docker ps -aq)
-docker rm $(docker ps -a -q) 或者 docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
-
